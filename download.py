a629b012daa213100d631d9113e01282245f167f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-03 18:22:30 +0100

    continue

--- download.py
+++ download.py
@@ -10,15 +10,27 @@ url = 'http://xkcd.com' # starting url
 os makedirs('xkcd', exists_ok=True) # store comics in ./xkcd
 
 while not url.endswith('#'):
-    # TODO: Download the page.
+    # Download the page.
+    print('Downloading page {}'.format(url))
     result = requests.get(url)
     result.raise_for_status()
 
     # TODO: Find the URL of the comic image.
     result_text = bs4.BeautifulSoup(result.text)
-    image_element = result_text.select('.comic img')
-     
+    image_element = result_text.select('#comic img')
+    if image_element == []:
+        print('Could not find comic image.')
+    else:
+        try:
+            comic_url = 'http:' + image_element[0].get('src')
+
     # TODO: Download the image.
+            print('Downloading image {}'.format(comic_url))
+            result = requests.get(comic_url)
+            result.raise_for_status()
+        except requests.exceptions.MissingSchema:
+                        
+
     # TODO: Save the image to ./xkcd.
     # TODO: Get the Prev button's url.
 
