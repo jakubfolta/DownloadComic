56e9a49d6331e612497e095185bae3b8ee4e5008
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-03 17:41:17 +0100

    continue

--- download.py
+++ download.py
@@ -6,7 +6,16 @@ import requests
 import os
 import bs4
 
+url = 'http://xkcd.com' # starting url
+os makedirs('xkcd', exists_ok=True) # store comics in ./xkcd
 
+while not url.endswith('#'):
+    # TODO: Download the page.
+    # TODO:
+    # TODO:
+    # TODO:
+    # TODO:
+    # TODO:
 
 
 # TODO: Change project status - github
