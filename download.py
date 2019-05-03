3ca7de5bb60aafda24ab2b787338fe74e5398ce2
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-03 20:52:29 +0100

    continue

--- download.py
+++ download.py
@@ -29,10 +29,21 @@ while not url.endswith('#'):
             result = requests.get(comic_url)
             result.raise_for_status()
         except requests.exceptions.MissingSchema:
-                        
+            # Skip this comic.
+            prev_link = result_text.select('a[rel="prev"]')[0]
+            url = 'http://xkcd.com' + prev_link.get('href')
+            continue
+
 
     # TODO: Save the image to ./xkcd.
+        image_file = open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb')
+        for chunk in result.iter_content(100000):
+            image_file.write(chunk)
+        image_file.close()
+
     # TODO: Get the Prev button's url.
+    prev_link = result_text.select('s[rel="prev"]')[0]
+    url = 'http://xkcd.com' + prev_link.get('href')
 
 print('Done.')
 
