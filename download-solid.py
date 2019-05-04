2311d50a4f870c949193e6de03182a9cd4f28017
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-04 16:26:10 +0100

    continue

---
+++ download-solid.py
@@ -0,0 +1,11 @@
+#! python3
+
+"""download-solid.py - Download every single XKCD comic."""
+
+# TODO: Import essential modules.
+# TODO: Set basic url and create directory to save comics.
+# TODO:
+# TODO:
+# TODO:
+# TODO:
+# TODO:
