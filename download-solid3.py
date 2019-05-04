a6af61c46a393f550b234a6307fa49af46324419
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-04 20:43:58 +0100

    continue

--- download-solid3.py
+++ download-solid3.py
@@ -2,9 +2,20 @@
 
 """download-solid3.py - Download every single XKCD comic."""
 
-# TODO: Import essential modules.
-# TODO: Set basic url to download and directory to save comics.
-# TODO: In a while loop download webpage.
+# Import essential modules.
+import os
+import requests
+import bs4
+
+# Set basic url to download and directory to save comics.
+url = 'http://xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+
+# In a while loop download webpage.
+while not url.endswith('#'):
+    result = requests.get(url)
+    result.raise_for_status()
+
 # TODO: Parse webpage and retrieve image elements using bs4 module.
 # TODO: Set comic url to download.
 # TODO: Download comic using requests module.
