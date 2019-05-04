83c5b20400e5d0d79371bbe2a96fa5e99c3fdf1c
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-04 18:45:48 +0100

    continue

--- download-solid.py
+++ download-solid.py
@@ -2,10 +2,34 @@
 
 """download-solid.py - Download every single XKCD comic."""
 
-# TODO: Import essential modules.
-# TODO: Set basic url and create directory to save comics.
-# TODO: Download webpage.
+# Import essential modules.
+import os
+import requests
+import bs4
+import logging
+
+logging.basicConfig(level = logging.DEBUG, format = '%(levelname)s, %(message)s')
+#logging.disable(logging.CRITICAL)
+
+# Set basic url and create directory to save comics.
+url = 'http://xkcd.com'
+os.makedirs('xkcd', exist_ok=True)
+
+# Download webpage.
+while not url.endswith('1'):
+    print('Downloading page: {}'.format(url))
+    result = requests.get(url)
+    result.raise_for_status()
+
 # TODO: Parse website and retrieve image url element.
+    result_text = bs4.BeautifulSoup(result.text)
+    image_element = result_text.select('#comic img')
+    if image_element == []:
+        print('Could not find comic image.')
+    else:
+        try:
+            comic_url = 'http:' + image_element
+
 # TODO: Download comic.
 # TODO: Save it to a file.
 # TODO: Change url to previous page.
