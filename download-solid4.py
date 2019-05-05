3b50c622484471f8a89393a0a4e5d68288330311
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-05 09:50:28 +0100

    continue

--- download-solid4.py
+++ download-solid4.py
@@ -4,9 +4,8 @@
 
 # TODO: Import essential modules.
 # TODO: Set basic url to download and create directory to save comics.
-# TODO: Download webpage using requests module and immediately check if there wasn't any error while downloading.
-# TODO: 
-# TODO:
-# TODO:
-# TODO:
-# TODO:
+# TODO: In a while loop download webpage using requests module, check for error.
+# TODO: Parse downloaded webpage and retrieve image element using bs4 module.
+# TODO: Create comic url, download it. If error occurs go to next comic.
+# TODO: Save comic to new folder.
+# TODO: Set new url with next comic and repeat.
