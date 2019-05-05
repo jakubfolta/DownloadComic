b8da579172094527405e26f26e9d6bcb77eb97d4
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-05 10:52:17 +0100

    continue

--- download-solid4.py
+++ download-solid4.py
@@ -13,10 +13,36 @@ os.makedirs('xkcd', exist_ok=True)
 
 # In a while loop download webpage using requests module, check for error.
 while not url.endswith('#'):
+    print('Downloading page {}'.format(url))
     result = requests.get(url)
     result.raise_for_status()
 
-# TODO: Parse downloaded webpage and retrieve image element using bs4 module.
-# TODO: Create comic url, download it. If error occurs go to next comic.
-# TODO: Save comic to new folder.
-# TODO: Set new url with next comic and repeat.
+# Parse downloaded webpage and retrieve image element using bs4 module.
+    result_text = bs4.BeautifulSoup(result.text)
+    image_element = result_text.select('#comic img')
+    if image_element == []:
+        print('Could not find image!')
+    else:
+        try:
+
+# Create comic url, download it. If error occurs go to next comic.
+            comic_url = 'http:' + image_element[0].get('src')
+            print('Downloading comic ---------- {}'.format(os))
+            result = requests.get(comic_url)
+            result.raise_for_status()
+        except requests.exceptions.MissingSchema:
+            prev_link = result_text.select('a[rel="prev"]')[0]
+            url = 'http://xkcd.com' + prev_link.get('href')
+            continue
+
+# Save comic to new folder.
+        image_file = open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb')
+        for chunk in result.iter_content(100000):
+            image_file.write(chunk)
+        image_file.close()
+
+# Set new url with next comic and repeat.
+    prev_link = result_text.select('a[rel="prev"]')[0]
+    url = 'http://xkcd.com' + prev_link.get('href')
+
+print('All comics downloaded!')
